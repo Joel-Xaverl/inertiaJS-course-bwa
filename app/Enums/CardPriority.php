594ad4b1538aa1f5a48ecb0c13c02dd9f3805f6e
Joel Xaverl <joxabelajar@gmail.com>
2025-01-20 15:38:00 +0700

    model and migration card

---
+++ app/Enums/CardPriority.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Enums;
+
+
+enum CardPriority: string
+{
+    case URGENT = 'Urgent';
+    case HIGH = 'High';
+    case MEDIUM = 'Medium';
+    case LOW = 'Low';
+    case UNKNOWN = 'Unknown';
+
+    public static function option(): array
+    {
+        return collect(self::cases())->map(fn($item) => [
+            'value' => $item->value,
+            'label' => $item->name,
+        ])->values()->toArray();
+    }
+}
